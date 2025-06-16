7d4e86eed1fe4bc0d7fa1eed10b2c80b37f9b9fc
syahrilhanla <syahrilhanla5@gmail.com>
2025-06-16 14:11:25 +0700

    feat: integrate login form with API

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { Toaster } from "@/components/ui/sonner";
+
 const inter = Inter({
 	variable: "--font-inter",
 	subsets: ["latin"],
@@ -20,7 +22,8 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${inter.variable} font-sans antialiased`}>
-				{children}
+				<main>{children}</main>
+				<Toaster richColors closeButton />
 			</body>
 		</html>
 	);
--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,34 +1,58 @@
 import { z } from "zod";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const loginSchema = z.object({
-	email: z.string().email("Invalid email address"),
+	username: z.string().min(2, "Username must be at least 2 characters"),
 	password: z.string().min(8, "Password must be at least 8 characters"),
 });
 
+type LoginFormData = z.infer<typeof loginSchema>;
+
 export default function LoginForm() {
+	const router = useRouter();
+
 	const {
 		register,
 		handleSubmit,
 		formState: { errors, isSubmitting },
-	} = useForm<z.infer<typeof loginSchema>>({
+	} = useForm<LoginFormData>({
 		resolver: zodResolver(loginSchema),
 	});
 
-	const onSubmitLogin = (data: any) => {
-		console.log("Login form submitted:", data);
+	const onSubmitLogin: SubmitHandler<LoginFormData> = async (data) => {
+		new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate network delay
+
+		try {
+			await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, data);
+
+			toast.success("Login successful!");
+			router.push("/articles");
+		} catch (error) {
+			const errorMessage = axios.isAxiosError(error)
+				? error.response?.data.error
+				: error;
+
+			if (errorMessage) {
+				toast.error(errorMessage);
+			} else {
+				toast.error("Login failed. Please try again later.");
+			}
+		}
 	};
 
 	return (
 		<form onSubmit={handleSubmit(onSubmitLogin)} className="space-y-4">
 			<div className="space-y-2">
-				<Input {...register("email")} placeholder="Email" />
-				{errors.email && (
-					<p className="text-red-500 text-sm">{errors.email.message}</p>
+				<Input {...register("username")} placeholder="Username" />
+				{errors.username && (
+					<p className="text-red-500 text-sm">{errors.username.message}</p>
 				)}
 			</div>
 			<div className="space-y-2">
@@ -42,7 +66,11 @@ export default function LoginForm() {
 				)}
 			</div>
 
-			<Button type="submit" className="w-full">
+			<Button
+				disabled={isSubmitting}
+				type="submit"
+				className="w-full disabled:opacity-50 disabled:cursor-not-allowed"
+			>
 				Login
 			</Button>
 		</form>
