37da6e4e7020b1c11977e216eba7651f31d45c17
syahrilhanla <syahrilhanla5@gmail.com>
2025-06-17 15:08:37 +0700

    fix: add eslint-disable comment for exhaustive-deps rule in ArticleSearchInput

--- src/components/Article/ArticleSearchInput.tsx
+++ src/components/Article/ArticleSearchInput.tsx
@@ -30,6 +30,7 @@ const ArticleSearchInput = () => {
 
 	useEffect(() => {
 		updateSearchParams();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [debouncedSearch, updateSearchParams]);
 
 	return (
