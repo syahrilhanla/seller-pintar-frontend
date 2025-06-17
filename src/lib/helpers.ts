a8a5c21516a09e487e3d42c6049c0a9f854d64f9
syahrilhanla <syahrilhanla5@gmail.com>
2025-06-17 20:24:19 +0700

    feat: enhance formatDate function to support full time formatting

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,9 +1,24 @@
-export const formatDate = (date: Date) => {
+export const formatDate = (date: Date, fullTime?: boolean) => {
+  if (fullTime) {
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+    };
+    // Format: April 13, 2025 10:55:12
+    const formatted = new Intl.DateTimeFormat("en-US", options).format(date);
+    return formatted.replace(",", "");
+  }
+
+  // Default: only date
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
     month: "long",
     day: "numeric",
   };
-
   return new Intl.DateTimeFormat("en-US", options).format(date);
 };
