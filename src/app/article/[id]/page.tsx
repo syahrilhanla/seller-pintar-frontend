3da8a4dc328b9d10825f224e9833672f397e23f3
syahrilhanla <syahrilhanla5@gmail.com>
2025-06-17 14:55:30 +0700

    feat: implement article detail page with related articles

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,5 +1,66 @@
-const ArticleDetailPage = () => {
-	return <div>ArticleDetailPage</div>;
+import ArticleCard from "@/components/Article/ArticleCard";
+import { formatDate } from "@/lib/helpers";
+import { Article } from "@/types/article.type";
+import axios from "axios";
+import { Dot } from "lucide-react";
+import Image from "next/image";
+
+interface Props {
+	params: Promise<{ id: string }>;
+}
+
+const ArticleDetailPage = async ({ params: params }: Props) => {
+	const { id } = await params;
+
+	const { data } = await axios.get(
+		`${process.env.NEXT_PUBLIC_API_URL}/articles/${id}`
+	);
+
+	const article = data as Article;
+	const category = article.categoryId;
+
+	const { data: relatedArticles } = await axios.get(
+		`${process.env.NEXT_PUBLIC_API_URL}/articles?category=${category}&limit=3`
+	);
+
+	return (
+		<div className="max-w-5xl mx-auto mt-12 px-4 gap-4 py-8 flex flex-col items-center">
+			<p className=" text-sm md:text-base text-slate-600 flex gap-1 items-center">
+				{formatDate(new Date(article.createdAt))} <Dot /> Created by{" "}
+				{article.user.username}
+			</p>
+
+			<h1 className="text-3xl font-semibold text-slate-900 text-center">
+				{article.title}
+			</h1>
+
+			<Image
+				src={article.imageUrl || "/placeholder-image.png"}
+				alt={article.title}
+				className="mt-4 w-full h-auto rounded-lg"
+				width={0}
+				height={0}
+				sizes="100vw"
+			/>
+
+			<main className="w-full my-4">
+				<p
+					className="text-gray-700 text-justify"
+					dangerouslySetInnerHTML={{ __html: article.content }}
+				/>
+			</main>
+
+			<section className="w-full mt-4 px-8">
+				<h3 className="text-2xl text-slate-900">Other articles</h3>
+
+				<div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+					{relatedArticles.data.map((relatedArticle: Article) => (
+						<ArticleCard article={relatedArticle} key={relatedArticle.id} />
+					))}
+				</div>
+			</section>
+		</div>
+	);
 };
 
 export default ArticleDetailPage;
