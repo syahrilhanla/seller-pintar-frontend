cd714d0de5b2738d4691027c28abd8b8ccf2dadf
syahrilhanla <syahrilhanla5@gmail.com>
2025-06-18 10:12:43 +0700

    fix: add serverExternalPackages to suppress yjs warning in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["yjs"], // to suppress warning about yjs not being a server component (issued in TipTap docs)
   images: {
     remotePatterns: [
       {
