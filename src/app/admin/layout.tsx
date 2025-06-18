79d1f7412a263ddde31eff6b79e4ec4fe3db696e
syahrilhanla <syahrilhanla5@gmail.com>
2025-06-18 10:25:18 +0700

    feat: add SidebarTrigger component to AdminLayout for improved navigation

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,7 +2,7 @@
 
 import Navbar from "@/components/Navbar/Navbar";
 import { AppSidebar } from "@/components/ui/app-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { usePathname } from "next/navigation";
 
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
@@ -24,6 +24,9 @@ const AdminLayout = ({ children }: { children: React.ReactNode }) => {
 
 				<main className="bg-gray-100 min-h-[calc(100dvh-4rem)] flex flex-col">
 					{/* <SidebarTrigger /> */}
+					<div className="fixed left-2 p-2 md:hidden">
+						<SidebarTrigger className="w-8 h-8 rounded-full p-3 bg-slate-200 cursor-pointer" />
+					</div>
 					{children}
 				</main>
 			</div>
