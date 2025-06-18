66de84d3ce9a0c89909a29459ee9cee3362ff2f4
syahrilhanla <syahrilhanla5@gmail.com>
2025-06-18 19:17:55 +0700

    feat: add searchParams prop to ArticleFormPage for article retrieval

--- src/app/admin/article-form/page.tsx
+++ src/app/admin/article-form/page.tsx
@@ -1,7 +1,24 @@
 import AdminArticleForm from "@/components/AdminArticle/AdminArticleForm";
+import { Article } from "@/types/article.type";
 import axios from "axios";
 
-const ArticleFormPage = async () => {
+interface Props {
+	searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+const ArticleFormPage = async ({ searchParams }: Props) => {
+	const { id } = await searchParams;
+
+	let article: Article | null = null;
+
+	if (id) {
+		const { data } = await axios.get(
+			`${process.env.NEXT_PUBLIC_API_URL}/articles/${id}`
+		);
+
+		article = data;
+	}
+
 	const categories = await axios.get(
 		`${process.env.NEXT_PUBLIC_API_URL}/categories?limit=100`
 	);
